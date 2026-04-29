417fc5b57fa12860d6c26e991a81c187151d193f
DJOMNANG EMMANUELLA <joycedjomnang@gmail.com>
2026-04-29 10:27:28 +0100

    Delete src directory

--- src/main/java/com/bibliotheque/emprunteur/exception/DuplicateEmprunteurException.java
+++
@@ -1,8 +0,0 @@
-package com.bibliotheque.emprunteur.exception;
-
-public class DuplicateEmprunteurException extends RuntimeException {
-
-    public DuplicateEmprunteurException(String email) {
-        super("Un emprunteur existe deja avec l'email : " + email);
-    }
-}
--- src/main/java/com/bibliotheque/emprunteur/exception/EmprunteurNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bibliotheque.emprunteur.exception;
-
-public class EmprunteurNotFoundException extends RuntimeException {
-
-    public EmprunteurNotFoundException(Long id) {
-        super("Aucun emprunteur trouve avec l'id : " + id);
-    }
-}
